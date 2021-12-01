9d18abb1a43a22734c4264bf134954b1c0bc8fed
s.fedyanin <s.fedyanin@skbkontur.ru>
2021-12-01 19:14:35 +0500

    Add Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,28 @@
 fun main() {
+    fun countNumOfLarger(values: List<Int>): Int {
+        var numOfLarger = 0
+        var prev = values[0]
+        for (value in values) {
+            if (value > prev) {
+                numOfLarger++
+            }
+            prev = value
+        }
+        return numOfLarger
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val values = input.map(Integer::parseInt)
+        return countNumOfLarger(values)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val values = input.map(Integer::parseInt)
+        val sums = (0..values.count() - 3).map { values[it] + values[it + 1] + values[it + 2] }
+        return countNumOfLarger(sums)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("input")
     println(part1(input))
     println(part2(input))
 }
